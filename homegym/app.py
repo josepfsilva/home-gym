10bfe01e6464d3f79be1544ab17372fe3224ef61
Leandro Costa <lrc@ua.pt>
2023-12-12 23:35:42 +0000

    inicio da est. de frontend - flask & bootstrap

--- homegym/app.py
+++ homegym/app.py
@@ -4,4 +4,9 @@ from views import mgvideos, mgamificacao, mgamigos, mgtreinos
 app = Flask(__name__)
 #app.config.from_object('config.py')
 
-  
+@app.route("/")
+def open()
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0', debug = True)
