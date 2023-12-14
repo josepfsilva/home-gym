887a82feb3b75d204e805152e575d6f1e9b94e95
josepfsilva <josepfsilva@ua.pt>
2023-12-14 15:43:32 +0000

    updates

--- homegym/app.py
+++ homegym/app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from views import mgvideos, mgamificacao, mgamigos, mgtreinos
 
+
 app = Flask(__name__)
 #app.config.from_object('config.py')
 
 @app.route("/")
-def open()
+def open():
     return render_template('index.html')
 
 if __name__ == "__main__":
