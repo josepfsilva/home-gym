9cbe2edfb11853a4a11e4da26669e1b2a5cc045d
josepfsilva <josepfsilva@ua.pt>
2023-12-18 16:52:42 +0000

    yo

--- homegym/app.py
+++ homegym/app.py
@@ -2,57 +2,23 @@ from datetime import date
 from flask import Flask, render_template
 import sqlite3
 from views import mgvideos, mgamificacao, mgamigos, mgtreinos
-from models import init_db,clear_db
+from models import init_db,clear_db,add_exercises,add_user
 
 app = Flask(__name__)
 #app.config.from_object('config.py')
 
 
-def add_user():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('database.db')
-    c = conn.cursor()
-
-    # Insert a new row of data into the Users table
-    c.execute("""
-        INSERT INTO Users (Username, Password, Email, BirthDate, RegistrationDate, Role, Status)
-        VALUES (?, ?, ?, ?, ?, ?, ?)
-    """, ('João', 'testpassword', 'testuser@example.com', '2000-01-01', date.today(), 'User', 'Online'))
-
-    # Commit the changes and close the connection
-    conn.commit()
-    conn.close()
-
-def add_exercises():
-    # Connect to the SQLite database
-    db = sqlite3.connect('database.db')
-    cursor = db.cursor()
-
-    cursor.execute("INSERT INTO Exercises (Name, Description, URL, Type, Difficulty) VALUES ('Push-ups', 'A basic exercise for upper body strength.', 'https://www.youtube.com/watch?v=euPXf2hqU3s', 'Musculacao', 'Easy');")
-    cursor.execute("INSERT INTO Exercises (Name, Description, URL, Type, Difficulty) VALUES ('Sit-ups', 'An exercise for strengthening the abdominal muscles.', 'http://example.com/sit-ups', 'Musculacao', 'Medium');")
-    cursor.execute("INSERT INTO Exercises (Name, Description, URL, Type, Difficulty) VALUES ('Squats', 'A lower body exercise targeting the thighs and buttocks.', 'http://example.com/squats', 'Musculacao', 'Hard');")
-    cursor.execute("INSERT INTO ExercisePlan (Exercise1, Exercise2, Exercise3) VALUES (1, 2, 3);")
-    
-    db.commit()
-    db.close()
-
-
-
-
 with app.app_context():
     init_db()
     #clear_db()
-    add_exercises()
-    add_user()
+    #add_exercises()
+    #add_user()
     
+ 
 
+@app.route("/menu" , methods=['GET', 'POST'])
+def menu():
     
-    
-
-
-
-@app.route("/" , methods=['GET', 'POST'])
-def open():
     db = sqlite3.connect('database.db')
     cursor = db.cursor()
     cursor.execute("""SELECT Username
@@ -61,8 +27,6 @@ def open():
                    """)
     user = cursor.fetchone()[0]
     db.close()
-    
-    
     
     db = sqlite3.connect('database.db')
     cursor = db.cursor() 
@@ -82,12 +46,9 @@ def open():
     
     exercises = [exercise_data[i:i+4] for i in range(0,len(exercise_data),4)]
     
-    return render_template('index.html', exercises = exercises, user = user)
+    return render_template('menuInicial.html', exercises = exercises, user = user)
     
-def convert_to_embed_url(url):
-    video_id = url.split('v=')[1]
-    embed_url = 'https://www.youtube.com/embed/' + video_id
-    return embed_url
+
 
 @app.route("/exercise")
 def exercise():
@@ -101,7 +62,7 @@ def exercise():
     """,)
 
     url = cursor.fetchone()[0]
-    embed_url = convert_to_embed_url(url)
+    embed_url = mgvideos.convert_to_embed_url(url)
     db.close()
 
     return render_template('exercise.html', url=embed_url)
