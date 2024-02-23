cd6046ab6409e262fa1230d74e10a54682779374
adrianocosta041 <adrianorcosta@ua.pt>
2024-02-23 21:21:51 +0000

    All images and start button added

--- homegym/static/js/planox.js
+++ homegym/static/js/planox.js
@@ -123,8 +123,9 @@ function loadPlanInfo(planNumber) {
                         console.log('../' + exerciseDetails[2]);
                         //html += 'div class="ExImage"><img src=../' + exerciseDetails[] + ' class="Eximg" ></div>';
                     }
-                    html += '</div>';
+                    html += '</div></div>';
                     //fim dos exercicios
+                    html += '<button class="button"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" height="24" fill="none" class="svg-icon"><g stroke-width="2" stroke-linecap="round" stroke="#ffffff"><rect y="3" x="9" width="6" rx="3" height="11"></rect><path d="m12 18v3"></path><path d="m8 21h8"></path><path d="m19 11c0 3.866-3.134 7-7 7-3.86599 0-7-3.134-7-7"></path></g></svg><span class="lable">Iniciar</span></button > '
 
                     container.append(html);
                 })
