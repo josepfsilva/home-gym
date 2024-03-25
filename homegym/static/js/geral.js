8604b33ea4728deadd42a5e3e882bb54f429261c
adrianocosta041 <adrianorcosta@ua.pt>
2024-03-25 23:47:56 +0000

    minor update to the level text

--- homegym/static/js/geral.js
+++ homegym/static/js/geral.js
@@ -1,4 +1,4 @@
-function showLevel(){
+function showLevel() {
     fetch('http://127.0.0.1:5000/getlevel')
         .then(response => {
             if (!response.ok) {
@@ -9,12 +9,12 @@ function showLevel(){
         .then(data => {
             console.log(data);
             var container = $('#level');
-            container.empty();      
-            var html = '<div class="leveltext"> Nivel ' + data + ' <img src="../static/img/star.png" alt="Icon" style="width: 20px; height: 20px;"></div>';
+            container.empty();
+            var html = '<div class="leveltext"> Nivel ' + data + ' <i class="fa-solid fa-star" style="color: #FFD43B;"></i></div>';
             container.append(html);
         })
         .catch(error => {
             console.error('Fetch error:', error);
             return Promise.reject(error);
-    });
+        });
 }
